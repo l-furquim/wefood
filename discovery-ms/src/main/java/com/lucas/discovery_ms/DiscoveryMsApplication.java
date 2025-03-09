72b8758383120bbd005178913ebb10722a3377f9
l-furquim <lucas.furquim@sptech.school>
2025-03-09 17:03:50 -0300

    add start of discovery-ms

---
+++ discovery-ms/src/main/java/com/lucas/discovery_ms/DiscoveryMsApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.discovery_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryMsApplication.class, args);
+	}
+
+}
