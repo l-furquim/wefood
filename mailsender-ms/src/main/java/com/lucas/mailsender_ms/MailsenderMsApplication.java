07cf39ec7cec7b0d1cff27aa84c9be1368881adf
l-furquim <lucas.furquim@sptech.school>
2025-03-11 17:57:28 -0300

    add mailsender ms

---
+++ mailsender-ms/src/main/java/com/lucas/mailsender_ms/MailsenderMsApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.mailsender_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderMsApplication.class, args);
+	}
+
+}
---
+++ mailsender-ms/src/test/java/com/lucas/mailsender_ms/MailsenderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.mailsender_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailsenderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
