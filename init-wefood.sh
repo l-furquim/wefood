8873b8f80ed6539e2b72f3208c5bbea75499a09e
l-furquim <lucas.furquim@sptech.school>
2025-04-19 10:32:06 -0300

    change the drop wefood containers for a better name

--- init-wefood.sh
+++ init-wefood.sh
@@ -3,3 +3,13 @@
 sudo docker start wefood-postgres
 sudo docker start wefood-zookeeper
 sudo docker start wefood-redis
+sudo docker start wefood-kafka
+
+sudo docker start wefood-discovery
+sudo docker start wefood-profile
+sudo docker start wefood-notification
+sudo docker start wefood-mailsender
+sudo docker start wefood-gateway
+sudo docker start wefood-payment
+sudo docker start wefood-pedido
+
